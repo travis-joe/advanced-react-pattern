969d36e6c56610a6579e42cd7669cfe61964c50a
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-05-31 17:55:51 +0800

    8

--- src/exercises/08.js
+++ src/exercises/08.js
@@ -29,6 +29,16 @@ class Toggle extends React.Component {
   }
   initialState = {on: this.props.initialOn}
   state = this.initialState
+  internalSetState = (changes, callback) => {
+    this.setState(currentState => {
+      const changesObj =
+        typeof changes === 'function'
+          ? changes(currentState)
+          : changes
+      const reducedChanges = this.props.stateReducer(currentState, changesObj)
+      return Object.keys(reducedChanges).length ? reducedChanges : null
+    }, callback)
+  }
   // 🐨 let's add a method here called `internalSetState`. It will simulate
   // the same API as `setState(updates, callback)`:
   // - updater: (changes object or function that returns the changes object)
@@ -46,11 +56,11 @@ class Toggle extends React.Component {
   // 🐨 Finally, update all pre-existing instances of this.setState
   // to this.internalSetState
   reset = () =>
-    this.setState(this.initialState, () =>
+    this.internalSetState(this.initialState, () =>
       this.props.onReset(this.state.on),
     )
   toggle = () =>
-    this.setState(
+    this.internalSetState(
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
